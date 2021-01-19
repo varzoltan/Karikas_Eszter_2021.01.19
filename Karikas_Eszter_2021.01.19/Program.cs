5ddd49e30ac962609c0bef9bfdd5bb4dfbc44d2a
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-01-19 15:19:11 +0100

    15:19

--- Karikas_Eszter_2021.01.19/Program.cs
+++ Karikas_Eszter_2021.01.19/Program.cs
@@ -10,6 +10,12 @@ namespace Karikas_Eszter_2021._01._19
     {
         static void Main(string[] args)
         {
+            //Készíts programot, mely képernyőre írja a kettes szám első 32 hatványát
+            int l = 2;
+            for (int j = 1;j<=32;j++)
+            {
+
+            }
         }
     }
 }
